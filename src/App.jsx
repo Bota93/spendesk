3c10cf0251c31796a1be4d841d8123b23e31c603
Bota93 <a.alcaraz.rodri@gmail.com>
2025-09-16 15:53:56 +0200

    feat: Añadir estructura de carpetas y sistema de rutas de react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import DashboardPage from './pages/DashboardPage';
+
 import './App.css'
 
-function App() {
 
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+
+function App() {
+    return (
+        <Router>
+          <div className="min-h-screen bg-gray-900 text-white p-8">
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/dashboard" element={<DashboardPage />} />
+            </Routes>
+          </div>
+        </Router>
+    );
 }
 
-export default App
+export default App;
+
---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function RegisterPage() {
+    return (
+        <div>
+            <h1 className='text-3xl font-bold underline'> Página de Registro</h1>
+        </div>
+    );
+}
+
+export default RegisterPage;
